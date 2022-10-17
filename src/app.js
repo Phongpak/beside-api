6cf17b018ba19d5b43b6b755159bb7c9c89e912f
BLXCKZ13\crowz <crowz.gac@gmail.com>
2022-10-17 16:18:28 +0700

    app guildlines

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
-const express = require("express");
-const { sequelize } = require("./models");
-sequelize.sync();
+//sync or create database tables
+//
+// const { sequelize } = require("./models");
+// sequelize.sync({ alter: true });
+//
+
+//core imports
+require("dotenv").config(); // for security
+const express = require("express"); //for API
+const cors = require("cors"); // for cross-domain-ports requests
+const morgan = require("morgan"); // for dev-using logs
+
+//routes import
+const xxxRoute = require("");
+
+//middlewares import
+const notFound = require("../src/middlewares/notFound");
+const error = require("../src/middlewares/error");
+
+//cores
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); //logs option dev or combined
+}
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/xxx", xxxRoute);
+
+//middlewares
+app.use(notFound);
+app.use(error);
+
+//server runner
+app.listen(process.env.PORT, () => {
+  console.log(`app server listening on port ${process.env.PORT}`);
+});
