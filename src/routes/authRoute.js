315b4b34c1be99455224fd6e380b06c305c5c057
Phongpak <phongpak@hotmail.com>
2022-10-18 12:25:52 +0700

    create auth route, controller, appError

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authController = require('../controller/authController');
+const authenticate = require('../middlewares/authenticate');
 
 const router = express.Router();
 
 router.post('/register', authController.register);
-
+router.post('/login', authController.login);
+router.get('/user', authenticate, authController.getUser);
 module.exports = router;
