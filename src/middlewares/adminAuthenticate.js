11f46bd0a7fa5e8dd637ef8800eb5f6494c9c36a
BLXCKZ13\crowz <crowz.gac@gmail.com>
2022-10-21 10:55:27 +0700

    add adminAuth

--- src/middlewares/adminAuthenticate.js
+++ src/middlewares/adminAuthenticate.js
@@ -1,18 +1,18 @@
-const { createError } = require("../utils/appError");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
+const AppError = require("../utils/appError");
 
 module.exports = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
     if (!authorization || !authorization.startsWith("Bearer")) {
       console.log("this is header", authorization);
-      createError("unauthenticated1", 401);
+      throw new AppError("unauthenticated1", 401);
     }
 
     const token = authorization.split(" ")[1];
     if (!token) {
-      createError("unauthenticated2", 401);
+      throw new AppError("unauthenticated2", 401);
     }
 
     const payload = jwt.verify(
@@ -26,10 +26,10 @@ module.exports = async (req, res, next) => {
     });
 
     if (!user) {
-      createError("unauthenticated3", 401);
+      throw new AppError("unauthenticated3", 401);
     }
     if (user.admin === false) {
-      createError("unauthenticated4", 401);
+      throw new AppError("unauthenticated4", 401);
     }
     req.user = user;
     next();
