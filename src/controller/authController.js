b6a6088e15f6e8b3bac67a136289e1fed92df6fe
Phongpak <phongpak@hotmail.com>
2022-10-24 17:26:07 +0700

    commit for pull

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,12 +1,12 @@
-const { User } = require("../models");
-const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const validator = require("validator");
-const AppError = require("../utils/appError");
+const { User } = require('../models');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const AppError = require('../utils/appError');
 const genToken = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET_KEY || "private_key", {
-    expiresIn: process.env.JWT_EXPIRES || "1d",
+  jwt.sign(payload, process.env.JWT_SECRET_KEY || 'private_key', {
+    expiresIn: process.env.JWT_EXPIRES || '1d',
   });
 
 exports.register = async (req, res, next) => {
@@ -46,27 +46,24 @@ exports.register = async (req, res, next) => {
       },
     });
     if (existingUser) {
-
-      console.log("test", existingUser);
-      throw new AppError("User already existed", 400);
+      console.log('test', existingUser);
+      throw new AppError('User already existed', 400);
     }
 
     const isEmail = validator.isEmail(user.email);
-    const isMobile = validator.isMobilePhone(user.mobile, ["th-TH"]);
+    const isMobile = validator.isMobilePhone(user.mobile, ['th-TH']);
     console.log(isEmail);
 
     if (!isEmail || !isMobile) {
-      throw new AppError("Invalid email or mobile");
+      throw new AppError('Invalid email or mobile');
     }
 
     const hashedPassword = await bcrypt.hash(user.password, 12);
     user.password = hashedPassword;
 
-
     await User.create(user);
 
     res.status(200).json({ message: 'Register success' });
-
   } catch (err) {
     next(err);
   }
@@ -76,20 +73,20 @@ exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    if (typeof email !== "string") {
-      throw new AppError("email address  password is invalid", 400);
+    if (typeof email !== 'string') {
+      throw new AppError('email address  password is invalid', 400);
     }
 
     const user = await User.findOne({
       where: { email: email },
     });
     if (!user) {
-      throw new AppError("email address or password is invalid", 400);
+      throw new AppError('email address or password is invalid', 400);
     }
 
     const isCorrect = await bcrypt.compare(password, user.password);
     if (!isCorrect) {
-      throw new AppError("email address or password is invalid", 400);
+      throw new AppError('email address or password is invalid', 400);
     }
     const token = genToken({ id: user.id });
     res.status(201).json({ token });
