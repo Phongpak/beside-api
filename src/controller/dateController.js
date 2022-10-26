3c5929d4cd6140c249d247f1fa2b1628b4a95ba8
BLXCKZ13\crowz <crowz.gac@gmail.com>
2022-10-26 13:17:00 +0700

    add update ,get orders and get dates

--- src/controller/dateController.js
+++ src/controller/dateController.js
@@ -3,16 +3,15 @@ const AppError = require("../utils/appError");
 const Moment = require("moment");
 const MomentRange = require("moment-range");
 const moment = MomentRange.extendMoment(Moment);
+const { Op } = require("sequelize");
 
 exports.dateCreate = async (req, res, next) => {
   try {
-    if (req.user.providerRequestStatus != "SUCCESS") {
-      throw new AppError("user is not provider");
-    }
     const { weekday, fromTime, toTime } = req.body;
     const check = await DateAvailable.findAll({
       where: { userId: req.user.id },
     });
+
     const create = await DateAvailable.create({
       weekday,
       fromTime,
@@ -25,9 +24,24 @@ exports.dateCreate = async (req, res, next) => {
     next(err);
   }
 };
-// const a = new Date(Date.now());
-// const d = a.getDay();
-// console.log(d);
+
+// wave เพิ่ม
+exports.getDateAvailable = async (req, res, next) => {
+  try {
+    const { weekday } = req.params;
+
+    const dateAvailable = await DateAvailable.findOne({
+      where: {
+        [Op.and]: [{ userId: req.user.id }, { weekday: weekday }],
+      },
+    });
+
+    res.status(200).json({ dateAvailable });
+  } catch (err) {
+    next(err);
+  }
+};
+// wave เพิ่ม
 
 exports.dateDelete = async (req, res, next) => {
   try {
@@ -47,15 +61,14 @@ exports.dateDelete = async (req, res, next) => {
 
 exports.dateUnavailableCreate = async (req, res, next) => {
   try {
-    const create = req.body.map((item) => {
-      return DateUnavailable.create({
-        unavailableDate: item.date,
-        fromTime: item.fromTime,
-        toTime: item.toTime,
-        userId: req.user.id,
-      });
+    const { unavailableDate, fromTime, toTime } = req.body;
+
+    const create = await DateUnavailable.create({
+      unavailableDate,
+      fromTime,
+      toTime,
+      userId: req.user.id,
     });
-    await Promise.all(create);
 
     res.status(201).json({ message: "create done" });
   } catch (err) {
@@ -71,10 +84,24 @@ exports.dateUnavailableDelete = async (req, res, next) => {
     if (selected.userId != req.user.id) {
       throw new AppError("cant delete others DateUnavailable", 400);
     }
-    await DateUnavailable.destroy({ where: { id } });
+    await DateUnavailable.destroy({ where: { id } }); // แก้ไขเป็น DateUnavailable
 
     res.status(201).json({ message: "delete done" });
   } catch (err) {
     next(err);
   }
 };
+
+// wave เพิ่ม
+exports.getUserUnavailableDate = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const unavailableDates = await DateUnavailable.findAll({
+      where: { userId: userId },
+    });
+
+    res.status(200).json({ unavailableDates });
+  } catch (err) {}
+};
+// wave เพิ่ม
